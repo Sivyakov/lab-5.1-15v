a8723e6635bb868d795c84eae274fc8746a0584a
UserXM230121 <UserXM230121@DESKTOP-591DAC7>
2021-12-22 22:23:20 +0600

    Добавьте файлы проекта.

---
+++ lab 5.1 15v/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace lab_5._1_15v
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             //5.1 базовый уровень 15v
+            Console.WriteLine("Введите строку ");
+            string a = Console.ReadLine();
+            int count = 0;
+            foreach (char c in a)
+                if (c == '('||c==')') count++;
+            Console.WriteLine("Количество круглых скобок:" + count);
+            foreach (char c in a)
+                if (c == ']' || c == '[') count++;
+            Console.WriteLine("Количество квадратных скобок:" + count);
+        }
+        }
+    }
+
